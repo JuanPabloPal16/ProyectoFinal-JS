5224bc95a95f1be01344d03bf77b22fe8c1f5811
Juan Pablo Pal <juanpablopal16@gmail.com>
2022-04-25 23:41:37 -0300

    ARREGLO EN EL JS

--- js/inicio.js
+++ js/inicio.js
@@ -10,28 +10,3 @@ function sweetAlert(){
         
     });
 }
-
-
-function productos (){
-    //BOTON REMERA
-     const boton1 = document.querySelector(".jean1")
-     boton1.addEventListener("click",()=>{
-     class Remera{
-         constructor(prenda, talles, precio, oferta){
-             this.prenda=prenda;
-             this.talles=talles;
-             this.precio=precio;      
-             this.oferta=oferta;
-             this.descuento=0;
-             this.calcularDescuento= (descuento) =>{this.precio=this.precio-descuento}
-         }
-     }
-         const jerryBlue = new Remera ("clasico jerry blue ","M,L,XL",1500,false)
-         console.log(jerryBlue)
-     
-         //LOCAL STORAGE
-         localStorage.setItem("jeanJerry", JSON.stringify(jerryBlue));
-         console.log(localStorage.getItem("jeanJerry"))
-     })
-
-}
